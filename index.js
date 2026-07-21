1f56e73041f097eb5461926d0ca2bcde2791fa96
eduar <dudummartins7@gmai.com>
2026-07-21 20:46:46 -0300

    feat: add UpdateUserController

--- index.js
+++ index.js
@@ -2,27 +2,36 @@ import 'dotenv/config.js';
 import express from 'express';
 import { CreateUserController } from './src/controllers/create-user.js';
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js';
+import { UpdateUserController } from './src/controllers/update-user.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/users', async (req, res) => {
+app.post('/api/users', async (request, response) => {
     const createUserController = new CreateUserController();
 
-    const { statusCode, body } = await createUserController.execute(req);
+    const { statusCode, body } = await createUserController.execute(request);
 
-    res.status(statusCode).json(body);
+    response.status(statusCode).send(body);
 });
 
-app.get('/api/users/:userId', async (req, res) => {
+app.patch('/api/users/:userId', async (request, response) => {
+    const updateUserController = new UpdateUserController();
+
+    const { statusCode, body } = await updateUserController.execute(request);
+
+    response.status(statusCode).send(body);
+});
+
+app.get('/api/users/:userId', async (request, response) => {
     const getUserByIdController = new GetUserByIdController();
 
-    const { statusCode, body } = await getUserByIdController.execute(req);
+    const { statusCode, body } = await getUserByIdController.execute(request);
 
-    res.status(statusCode).send(body);
+    response.status(statusCode).send(body);
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+app.listen(process.env.PORT, () =>
+    console.log(`Listening on port ${process.env.PORT}`),
+);
