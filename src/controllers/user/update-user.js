0c60222474400a285cad8405cf37373fd333c975
EduardoMartins <dudummartins7@gmail.com>
2026-08-31 20:43:46 -0300

    fix: fix CreateTransactionController required fields validation

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,40 +1,79 @@
-import bcrypt from 'bcrypt';
 import { EmailAlreadyInUseError } from '../../errors/user.js';
+import {
+    checkIfEmailIsValid,
+    checkIfIdIsValid,
+    checkIfPasswordIsValid,
+    emailIsAlreadyInUseResponse,
+    invalidIdResponse,
+    invalidPasswordResponse,
+    badRequest,
+    ok,
+    serverError,
+} from '../helpers/index.js';
 
-export class UpdateUserUseCase {
-    constructor(getUserByEmailRepository, updateUserRepository) {
-        this.getUserByEmailRepository = getUserByEmailRepository;
-        this.updateUserRepository = updateUserRepository;
+export class UpdateUserController {
+    constructor(updateUserUseCase) {
+        this.updateUserUseCase = updateUserUseCase;
     }
 
-    async execute(userId, updateUserParams) {
-        if (updateUserParams.email) {
-            const userWithProvidedEmail =
-                await this.getUserByEmailRepository.execute(
-                    updateUserParams.email,
-                );
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId;
 
-            if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
-                throw new EmailAlreadyInUseError(updateUserParams.email);
+            const isIdValid = checkIfIdIsValid(userId);
+
+            if (!isIdValid) {
+                return invalidIdResponse();
             }
-        }
 
-        const user = { ...updateUserParams };
+            const params = httpRequest.body;
 
-        if (updateUserParams.password) {
-            const hashedPassword = await bcrypt.hash(
-                updateUserParams.password,
-                10,
+            const allowedFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            ];
+
+            const someFieldIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
             );
 
-            user.password = hashedPassword;
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed.',
+                });
+            }
+
+            if (params.password) {
+                const passwordIsValid = checkIfPasswordIsValid(params.password);
+
+                if (!passwordIsValid) {
+                    return invalidPasswordResponse();
+                }
+            }
+
+            if (params.email) {
+                const emailIsValid = checkIfEmailIsValid(params.email);
+
+                if (!emailIsValid) {
+                    return emailIsAlreadyInUseResponse();
+                }
+            }
+
+            const updatedUser = await this.updateUserUseCase.execute(
+                userId,
+                params,
+            );
+
+            return ok(updatedUser);
+        } catch (error) {
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: error.message });
+            }
+
+            console.error(error);
+            return serverError();
         }
-
-        const updatedUser = await this.updateUserRepository.execute(
-            userId,
-            user,
-        );
-
-        return updatedUser;
     }
 }
